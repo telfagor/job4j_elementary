4e9c623e2b5ec28c33c4374245995ab986d39acf
Bolun Andrei <telfagor@gmail.com>
2021-08-19 21:37:45 +0300

    4.1.2. Глупый бот [#31323]

---
+++ src/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,24 @@
+package ru.job4j.condition;
+
+public class DummyBot {
+    public static String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String rsl = answer("Привет Бот.");
+        System.out.println(rsl);
+
+        rsl = answer("Пока.");
+        System.out.println(rsl);
+
+        rsl = answer("Я стану Java - разработчиком");
+        System.out.println(rsl);
+    }
+}
