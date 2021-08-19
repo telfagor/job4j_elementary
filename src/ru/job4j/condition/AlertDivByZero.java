93bcf484b94ac42610c5e2ae47d38450da537520
Bolun Andrei <telfagor@gmail.com>
2021-08-19 20:22:08 +0300

    4.1.0. Оператор if с блоком else [#227534]

--- src/ru/job4j/condition/AlertDivByZero.java
+++ src/ru/job4j/condition/AlertDivByZero.java
@@ -2,11 +2,15 @@ package ru.job4j.condition;
 
 public class AlertDivByZero {
     public static void main(String[] args) {
-        possibleDiv(10);
+        possibleDiv(0);
         possibleDiv(-10);
     }
 
     public static void possibleDiv(int number) {
+        if (number == 0) {
+            System.out.println("Could not div by 0.");
+        }
+
         if (number < 0) {
             System.out.println("This is negative number");
         }
