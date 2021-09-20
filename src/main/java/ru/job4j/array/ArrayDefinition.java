dce58f0672dcd21de07446acf9f69d08f1394516
Bolun Andrei <telfagor@gmail.com>
2021-09-20 21:55:03 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -15,5 +15,10 @@ public class ArrayDefinition {
         names[1] = "Igor";
         names[2] = "Alexey";
         names[3] = "Vasiliy";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
