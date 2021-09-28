e6fa714f6a8e59fc0953fa66597e4ab6dc8c1bfd
Bolun Andrei <telfagor@gmail.com>
2021-09-28 16:47:32 +0300

    Старые программы2

--- src/main/java/ru/job4j/calculator/Converter.java
+++ src/main/java/ru/job4j/calculator/Converter.java
@@ -3,7 +3,6 @@ package ru.job4j.calculator;
 public class Converter {
     public static float rubleToEuro(float value) {
         return value / 70;
-
     }
 
     public static float rubleToDollar(float value) {
