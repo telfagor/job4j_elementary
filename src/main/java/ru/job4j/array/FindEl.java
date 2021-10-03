b2cb7c1a654d5cde7b77d307583d29afa250abed
Bolun Andrei <telfagor@gmail.com>
2021-10-03 20:15:13 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/main/java/ru/job4j/array/FindEl.java
+++ src/main/java/ru/job4j/array/FindEl.java
@@ -3,14 +3,13 @@ package ru.job4j.array;
 public class FindEl {
     public static int indexOf(int[] data, int el, int start, int finish) {
         int rst = -1;
-        if ((start >= 0 && start < data.length) && (finish >= start && finish < data.length)) {
-            for (int i = start; i <= finish; i++) {
-                if (data[i] == el) {
-                    rst = i;
-                    break;
-                }
+        for (int i = start; i <= finish; i++) {
+            if (data[i] == el) {
+                rst = i;
+                break;
             }
         }
+
         return rst;
     }
 }
