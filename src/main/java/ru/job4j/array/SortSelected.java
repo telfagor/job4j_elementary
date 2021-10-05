d148821c93cfb343dac1081186663d5a31c4f8bf
Bolun Andrei <telfagor@gmail.com>
2021-10-05 21:32:56 +0300

    6.5.3. Сортировка выборкой. [#156320]

---
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+public class SortSelected {
+    public static void swap(int[] array, int min, int index, int i) {
+        int tmp = array[i];
+        array[i] = min;
+        array[index] = tmp;
+    }
+
+    public static int[] sort(int[] data) {
+        for (int i = 0; i < data.length; i++) {
+            int min = MinDiapason.findMin(data, i, data.length);
+            int index = FindEl.indexOf(data, min, i, data.length);
+            swap(data, min, index, i);
+        }
+        return data;
+    }
+}
---
+++ test/ru/job4j/array/SortSelectedTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class SortSelectedTest {
+
+    @Test
+    public void whenSortArrayWithLength5() {
+        int[] data = {3, 4, 1, 2, 5};
+        int[] result = SortSelected.sort(data);
+        int[] expected = {1, 2, 3, 4, 5};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenSortArrayWithLength3() {
+        int[] data = {3, 2, 1};
+        int[] result = SortSelected.sort(data);
+        int[] expected = {1, 2, 3};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenSortArrayWithLength7() {
+        int[] data = {-2, 0, 7, 100, 43, -6, 1000};
+        int[] result = SortSelected.sort(data);
+        int[] expected = {-6, -2, 0, 7, 43, 100, 1000};
+        Assert.assertArrayEquals(expected, result);
+    }
+}
